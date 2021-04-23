346c88cd5c41129652dd9a2b8b6a32b4c4005b7b
LepkoQQ <lepko.san@gmail.com>
2021-04-23 17:05:52 +0200

    work around css dependency bug with dynamic imports

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
-const ParlameterGeneric = () => import('./views/parlameter/Generic.vue');
-const ParlameterCircle = () => import('./views/parlameter/Circle.vue');
+import ParlameterGeneric from './views/parlameter/Generic.vue';
+import ParlameterCircle from './views/parlameter/Circle.vue';
+
+// TODO: Convert to dynamic import when this bug is fixed https://github.com/vitejs/vite/issues/3101
+// const ParlameterGeneric = () => import('./views/parlameter/Generic.vue');
+// const ParlameterCircle = () => import('./views/parlameter/Circle.vue');
 
 const routes = [
   { path: '/parlameter/generic', component: ParlameterGeneric },
