5db4442915c3d65c3bdb44dfa13b55136fd78bd6
LepkoQQ <lepko.san@gmail.com>
2021-04-23 20:12:28 +0200

    generate a list of valid routes on build and use them in the server

--- server/index.js
+++ server/index.js
@@ -1,33 +1,45 @@
 require('make-promises-safe');
 require('./load-env.js');
 const { createReadStream, existsSync } = require('fs');
-const { ensureDir } = require('fs-extra');
+const { ensureDir, readJSONSync } = require('fs-extra');
 const { resolve } = require('path');
 const { createHash } = require('crypto');
 const { fastify: createFastify } = require('fastify');
-const fastifyStatic = require('fastify-static');
 const { takeScreenshot } = require('./screenshot.js');
 
 const staticPath = resolve('./dist');
 const indexPath = resolve('./dist/index.html');
 const mediaPath = resolve('./media');
+const routeJson = readJSONSync('./dist/routes.json');
 
 const port = process.env.PORT || 3000;
 
 const fastify = createFastify({ logger: true });
 
-fastify.register(fastifyStatic, {
+fastify.register(require('fastify-sensible'));
+
+fastify.register(require('fastify-static'), {
   root: staticPath,
   prefix: '/',
   wildcard: false,
 });
 
 fastify.get('/*', async (request, reply) => {
+  const url = new URL(request.url, `http://localhost:${port}/`);
+
+  if (!routeJson.routes.includes(url.pathname)) {
+    reply.notFound();
+    return;
+  }
+
   const format = request.query.format || 'image';
+
   if (format === 'html') {
     reply.type('text/html').send(createReadStream(indexPath));
-  } else if (format === 'image') {
-    const url = new URL(request.url, `http://localhost:${port}/`);
+    return;
+  }
+
+  if (format === 'image') {
     url.searchParams.delete('format');
     const cacheKey = createHash('sha1').update(url.toString()).digest('hex');
     const imagePath = `${mediaPath}/${cacheKey}.png`;
@@ -42,13 +54,10 @@ fastify.get('/*', async (request, reply) => {
       });
     }
     reply.type('image/png').send(image);
-  } else {
-    reply.status(400).send({
-      message: `Invalid format: ${format}`,
-      error: 'Bad Request',
-      statusCode: 400,
-    });
+    return;
   }
+
+  reply.badRequest(`Invalid format: ${format}`);
 });
 
 fastify.listen(port, '0.0.0.0', (error) => {
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,34 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+function buildServerRoutes() {
+  return {
+    name: 'build-server-routes',
+    moduleParsed(moduleInfo) {
+      if (moduleInfo.id?.endsWith('routes.js')) {
+        const variableDeclarator = moduleInfo.ast.body.find(
+          (o) => o.type === 'VariableDeclaration'
+        )?.declarations?.[0];
+        if (variableDeclarator?.id?.name === 'routes') {
+          const elements = variableDeclarator.init?.elements || [];
+          const paths = elements
+            .map((e) => e?.properties.find((p) => p.key?.name === 'path'))
+            .map((p) => p.value?.value);
+          this.emitFile({
+            type: 'asset',
+            name: 'routes.json',
+            fileName: 'routes.json',
+            source: JSON.stringify({ routes: paths }, null, 2),
+          });
+        }
+      }
+    },
+  };
+}
+
 export default defineConfig({
   clearScreen: false,
-  plugins: [vue()],
+  plugins: [vue(), buildServerRoutes()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
